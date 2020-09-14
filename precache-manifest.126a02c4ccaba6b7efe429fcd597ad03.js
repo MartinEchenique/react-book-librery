e6c0df1ffbf41362c7238eff4ab035045e57f557
Martin Echenique <martinechenique94@gmail.com>
2020-09-14 20:05:03 -0300

    Updates

---
+++ precache-manifest.126a02c4ccaba6b7efe429fcd597ad03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f86e425f9630bb7fbde245e7649cb9c",
+    "url": "./public/index.html/index.html"
+  },
+  {
+    "revision": "0334e750f7d9ae894080",
+    "url": "./public/index.html/static/css/main.146298c1.chunk.css"
+  },
+  {
+    "revision": "7adcc8a1b0ad487ce3bc",
+    "url": "./public/index.html/static/js/2.79798e75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./public/index.html/static/js/2.79798e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0334e750f7d9ae894080",
+    "url": "./public/index.html/static/js/main.2b0a1756.chunk.js"
+  },
+  {
+    "revision": "c32066548d126b52db12",
+    "url": "./public/index.html/static/js/runtime-main.a698c0b1.js"
+  }
+]);
